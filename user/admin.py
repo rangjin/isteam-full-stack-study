a70e1e88ae92bef00c98f9e71e6b1a37d219254d
rangjin <heathaze2037@gmail.com>
2020-11-19 21:06:22 +0900

    Initial commit

---
+++ user/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User
+
+# Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'password')
+
+admin.site.register(User, UserAdmin)
